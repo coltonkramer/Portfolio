0c3cbc96540107f8b0ccf061489faf9b06607d1c
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2021-10-15 16:54:16 -0400

    fixed the c++ icon

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-function moveAcross(element){
-    var elStyle = window.getComputedStyle(element);
-    var TopValue = elStyle.getPropertyValue("top").replace("px","");
-    element.style.top = Number(topValue) + 20 + "px";
-}
-
-var title = document.getElementsByClassName("title")
-moveAcross(title);
 
